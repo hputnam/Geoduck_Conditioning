ff549f810ede3fa9d6e476e6328a84e49c455d86
Hollie Putnam <hollieputnam@gmail.com>
2018-10-05 12:50:10 -0400

    updated adult resp script and output

--- RAnalysis/Scripts/Adult_Respirometry.R
+++ RAnalysis/Scripts/Adult_Respirometry.R
@@ -1,7 +1,7 @@
 #Title: Respiration Calculations
 #Author: HM Putnam
 #Edited by: HM Putnam
-#Date Last Modified: 20180627
+#Date Last Modified: 20181005
 #See Readme file for details
 
 rm(list=ls()) #clears workspace 
@@ -38,7 +38,7 @@ library('dplyr')
 setwd("~/MyProjects/Geoduck_Conditioning/RAnalysis/") #set working
 
 ##### Respiration #####
-path.p<-"Data/Respirometry/" #the location of all your respirometry files 
+path.p<-"Data/Respirometry/Adult/" #the location of all your respirometry files 
 
 # bring in the respiration files
 file.names<-basename(list.files(path = path.p, pattern = "csv$", recursive = TRUE)) #list all csv file names in the folder and subfolders
@@ -47,24 +47,25 @@ file.names<-basename(list.files(path = path.p, pattern = "csv$", recursive = TRU
 file.names.full<-list.files(path = path.p, pattern = "csv$", recursive = TRUE) #list all csv file names in the folder and subfolders
 
 #generate a 3 column dataframe with specific column names
-Resp.R <- data.frame(matrix(NA, nrow=length(file.names)*2, ncol=5))
-colnames(Resp.R) <- c("Sample.ID","Intercept", "umol.L.sec","Temp.C","PR")
+Resp.R <- data.frame(matrix(NA, nrow=length(file.names), ncol=4))
+colnames(Resp.R) <- c("Sample.ID","Intercept", "umol.L.sec","Temp.C")
 
 #Load Sample Info
-Sample.Info <- read.csv(file="../Data/MetaData/Adult_Sample_Info.csv", header=T) #read sample.info data
+Sample.Info <- read.csv(file="Data/MetaData/Adult_Sample_Info.csv", header=T) #read sample.info data
 #includes treatment, tank, chamber volume, animal size etc for normalization
 
-#take the average of the two volumes measures for each sample
-Chamber.Vol.mL<-aggregate(Sample.Info$Volume~Sample.Info$Sample.ID, FUN=mean)
-colnames(Chamber.Vol.mL)<-c("Sample.ID","Volume")
 
 for(i in 1:length(file.names)) { # for every file in list start at the first and run this following function
-  Resp.Data1 <-read.table(file.path(path.p,file.names[i]), skip = 1, header=T, sep=",", na.string="NA", fill = TRUE, as.is=TRUE, fileEncoding="latin1") #reads in the data files
-  Resp.Data1  <- Resp.Data1[,c(2,9,16)] #subset columns of interest
-  Resp.Data1$Time <- as.POSIXct(Resp.Data1$Time,format="%H:%M:%S", tz = "") #convert time from character to time
-
-  Resp.Data   <-  thinData(Resp.Data , by=20)$newData1 #thin data by every 10 points
-  Resp.Data $sec <- as.numeric(rownames(Resp.Data )) #maintain numeric values for time
+  thin=30
+  a=0.4
+  rename <- sub("_.*", "", file.names[i])
+  Resp.Data <-read.table(file.path(path.p,file.names[i]), header=T, sep=",", na.string="NA", fill = TRUE, as.is=TRUE, fileEncoding="latin1") #reads in the data files
+  Resp.Data.orig  <- Resp.Data[,c(2,9,16)] #subset columns of interest
+  Resp.Data.orig$Time <- as.POSIXct(Resp.Data$Time,format="%H:%M:%S", tz = "") #convert time from character to time
+  Resp.Data   <-  thinData(Resp.Data.orig, by=thin)$newData1 #thin data 
+  Resp.Data$sec <- as.numeric(rownames(Resp.Data )) #maintain numeric values for time
+  Resp.Data$Temp <- NA # add a new column to fill with the thinned data
+  Resp.Data$Temp <-  thinData(Resp.Data.orig, xy=c(2,3),by=thin)$newData1[,2] #thin data  for the temp values
   plot(Value ~ sec, data=Resp.Data , xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE) #plot thinned data
   usr  <-  par('usr')
   rect(usr[1], usr[3], usr[2], usr[4], col='grey90', border=NA)
@@ -75,10 +76,41 @@ for(i in 1:length(file.names)) { # for every file in list start at the first and
   axis(2, las=1)
   dev.off()
   
-  Regs  <-  rankLocReg(xall=Resp.Data $sec, yall=Resp.Data $Value, alpha=0.3, 
+  Regs  <-  rankLocReg(xall=Resp.Data $sec, yall=Resp.Data $Value, alpha=a, 
                        method="pc", verbose=TRUE) 
-  pdf(paste0("~/Desktop/Data/20170822/T4_output",rename,"_",j,"regression.pdf"))
+  pdf(paste0("Output/",rename,"_regression.pdf"))
   plot(Regs)
   dev.off()
+
+  #s <- seq(0,nrow(Resp.R),length(file.names)) #to order the file output sequence in correct order in data frame
+  Resp.R[i,2:3] <- Regs$allRegs[1,c(4,5)] #inserts slope and intercept in the dataframe
+  Resp.R[i,1] <- paste0(rename) #stores the file name in the Date column
+  Resp.R[i,4] <- mean(Resp.Data$Temp, na.rm=T)  #stores the Temperature in the Temp.C column
+  
 }
+
+Resp.R 
+
+#Merge rates with sample info
+Data <- merge(Resp.R, Sample.Info, by="Sample.ID")
+
+#correct for the size of the chamber
+Data$micromol.s <- Data$umol.L.sec * (Data$Chamber.Vol.mL/1000)
+
+#calculate the average of the blanks from each time step
+blnks <- subset(Data, Sample.Type=="Blank")
+blnks <- aggregate(micromol.s ~ pH.Treatment, data=blnks, FUN=mean)
+Data <- merge(Data, blnks, by="pH.Treatment")
+colnames(Data)[colnames(Data) == 'micromol.s.x'] <- 'sample.micromol.s'
+colnames(Data)[colnames(Data) == 'micromol.s.y'] <- 'blank.micromol.s'
+
+#subtract the average of the blanks from each time step
+Data$corr.micromol.s <- Data$sample.micromol.s - Data$blank.micromol.s
+Data$micromol.bivol.s <- Data$corr.micromol.s/Data$Adult.Vol.ml
+Data$micromol.bivol.h <- Data$micromol.bivol.s*3600
+
+GeoResp <- cbind(Data$Date, Data$Sample.ID, Data$pH.Treatment, Data$Sample.Type, Data$micromol.bivol.h)
+
+write.csv(GeoResp,"~/MyProjects/Geoduck_Conditioning/RAnalysis/Output/Adult.Resp.Test.csv")
+
 
