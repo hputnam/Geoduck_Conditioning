a80ede0ce5ff153768b93f4c3f6cf04ffbf37aca
Hollie Putnam <hollieputnam@gmail.com>
2018-10-11 09:15:06 -0400

    updating analysis script and output plots

--- RAnalysis/Scripts/Stats.R
+++ RAnalysis/Scripts/Stats.R
@@ -14,6 +14,7 @@ library(ggpubr)
 library(nlme)
 library(plotrix)
 library(lsmeans)
+library(gridExtra)
 
 # Set Working Directory:
 setwd("~/MyProjects/Geoduck_Conditioning/RAnalysis/") #set working
@@ -32,15 +33,19 @@ size_EXP2.0 <-subset(size_EXP2.0, Day!=0)
 # Visualize experiment 1
 
 
-ggboxplot(size_EXP1, x = "Day", y = "shell_size", color = "treatment", ylab= "Size (mm)",
-          palette = c("#00AFBB", "#E7B800"))
+Exp1.Fig <- ggboxplot(size_EXP1, x = "Day", y = "shell_size", color = "treatment", ylab= "Shell Size (mm)",
+          palette = c("blue", "red"), main= "Initial Exposure")
 
-means.exp1 <- aggregate(shell_size ~ Day*treatment, data=size_EXP1, FUN=mean)
+Exp1.Fig
+
+x1 <- do.call(data.frame,aggregate(shell_size ~ Day*treatment, data = size_EXP1, function(x) c(mean = mean(x), se = std.error(x))))
 
 # Test for size differences in experiment 1
-Init.lme <- lmer(shell_size ~ treatment + (1|Day/treatment/tank), data = size_EXP1) 
+Init.lme <- lmer(shell_size ~ treatment*Day + (1|Day), data = size_EXP1) 
+anova(Init.lme)
+summary(Init.lme)
 
-m1 <- lme(shell_size~treatment,random=~1|Day/treatment/tank,data=size_EXP1)
+m1 <- lme(shell_size~treatment*Day,random=~1|Day,data=size_EXP1)
 anova(m1)
 
 
@@ -51,10 +56,16 @@ boxplot(residuals(m1)) #plot boxplot of residuals
 plot( fitted(m1),residuals(m1)) #display residuals versus fitter, normal QQ plot, leverage plot
 
 # NO STATISTICAL DIFFERENCE IN SIZE AFTER THE INITIAL EXPOSURE
-ggboxplot(size_EXP2, x = "Day", y = "shell_size", color = "treatment", ylab= "Size (mm)",
+
+
+#Exposure 2
+Exp2.Fig <- ggboxplot(size_EXP2, x = "Day", y = "shell_size", color = "treatment", ylab= "Size (mm)",
           palette = c("light blue", "darkblue", "pink", "red"))
+Exp2.Fig
+
 
 x2 <- do.call(data.frame,aggregate(shell_size ~ Day*Init.Trt*Sec.Trt, data = size_EXP2, function(x) c(mean = mean(x), se = std.error(x))))
+x2.1 <- do.call(data.frame,aggregate(shell_size ~ Init.Trt*Sec.Trt, data = size_EXP2.0, function(x) c(mean = mean(x), se = std.error(x))))
 
 
 m2 <- lme(shell_size~Init.Trt*Sec.Trt,random=~1|Day/Init.Trt/Sec.Trt,data=size_EXP2.0)
@@ -62,14 +73,13 @@ anova(m2)
 summary(m2)
 
 #Post hoc
-m2.lsm <- lsmeans(m2,specs=c("Init.Trt", "Sec.Trt"), adjust="tukey") #compute least-squares means for Treatment*Day from ANOVA model
-m2.lsm
+exp2.ph <- lsmeans(m2, pairwise ~ Init.Trt*Sec.Trt)
 
 par(mfrow=c(1,3)) #set plotting configuration
 par(mar=c(1,1,1,1)) #set margins for plots
-hist(residuals(m2.1)) #plot histogram of residuals
-boxplot(residuals(m2.1)) #plot boxplot of residuals
-plot( fitted(m2.1),residuals(m2.1)) #display residuals versus fitter, normal QQ plot, leverage plot
+hist(residuals(m2)) #plot histogram of residuals
+boxplot(residuals(m2)) #plot boxplot of residuals
+plot( fitted(m2),residuals(m2)) #display residuals versus fitter, normal QQ plot, leverage plot
 
 
 
@@ -181,3 +191,44 @@ Fig.Exp2.D6.size <- ggplot(Day6, aes(x=Sec.Trt, y=shell_size.mean, group=Init.Tr
                                   hjust = 0))
 
 Fig.Exp2.D6.size
+
+#Averaged across Days
+
+Fig.Exp2.All.size <- ggplot(x2.1, aes(x=Sec.Trt, y=shell_size.mean, group=Init.Trt)) + 
+  geom_errorbar(aes(ymin=x2.1$shell_size.mean-x2.1$shell_size.se, ymax=x2.1$shell_size.mean+x2.1$shell_size.se), colour="black", width=.1, position = position_dodge(width = 0.05)) +
+  geom_line(aes(linetype=Init.Trt), size = 0.5, position = position_dodge(width = 0.05)) +   
+  geom_point(aes(shape=Init.Trt), size = 3, position = position_dodge(width = 0.05)) +
+  #annotate("text", x=0.85, y=1.3, label = "b", size = 3) + #add text to the graphic for posthoc letters
+  #annotate("text", x=c(0.85,0.85,2.2,2.2,2.25), y=c(0.9,0.95,0.88, 0.92,0.97), label = "ab", size = 3) + #add text to the graphic for posthoc letters
+  xlab("Secondary Treatment") +
+  ylab("Shell size (mm)") +
+  ylim(5,7) +
+  theme_bw() + #Set the background color
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), #Set the text angle
+        axis.line = element_line(color = 'black'), #Set the axes color
+        panel.border = element_blank(), #Set the border
+        panel.grid.major = element_blank(), #Set the major gridlines
+        panel.grid.minor = element_blank(), #Set the minor gridlines
+        plot.background=element_blank(), #Set the plot background
+        legend.position='none') + #remove legend background
+  ggtitle(" Secondary Exposure All Days") +
+  theme(plot.title = element_text(face = 'bold', 
+                                  size = 12, 
+                                  hjust = 0))
+
+Fig.Exp2.All.size
+
+# Saving output plots
+
+Figure1.Exp1.Size <- arrangeGrob(Exp1.Fig, ncol=1)
+ggsave(file="Output/Geoduck_Size_Exp1.pdf", Figure1.Exp1.Size, width = 6.5, height = 8, units = c("in"))
+
+
+Figure1.Exp2.Size <- arrangeGrob(Exp2.Fig,Fig.Exp2.D0.size, Fig.Exp2.D2.size,Fig.Exp2.D4.size,Fig.Exp2.D6.size, ncol=1)
+ggsave(file="Output/Geoduck_Size_Exp2.byDay.pdf", Figure1.Exp2.Size, width = 6.5, height = 15, units = c("in"))
+
+Figure1.Exp2.AllSize <- arrangeGrob(Fig.Exp2.All.size, ncol=1)
+ggsave(file="Output/Geoduck_Size_Exp2.All.pdf", Figure1.Exp2.AllSize, width = 6.5, height = 8, units = c("in"))
+
+
+
